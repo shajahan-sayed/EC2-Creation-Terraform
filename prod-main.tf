417470fce5a5a951001ea0083a0d83e6acdb56b6
shajahan-sayed <shajahansayed111@gmail.com>
2025-10-25 20:53:18 +0530

    Update prod-main.tf

--- prod-main.tf
+++ prod-main.tf
@@ -33,7 +33,7 @@ egress {
 resource "aws_instance" "web-page"{
   ami           = var.ami_id
   instance_type = var.instance_type
-  subnet_id     = element(data.aws_subnet_ids.default.ids, 0)
+  subnet_id = var.subnet_id
   key_name      = var.key_name
   vpc_security_group_ids = [aws_security_group.prod-main.id]
   associate_public_ip_address = true
