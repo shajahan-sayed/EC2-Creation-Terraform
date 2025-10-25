ce632b1ea83a97c4e838b673b002b0dcab2d29ce
shajahan-sayed <shajahansayed111@gmail.com>
2025-10-25 20:26:23 +0530

    Update EC2-Variable.tf

--- EC2-Variable.tf
+++ EC2-Variable.tf
@@ -15,3 +15,12 @@ variable "Key_pair" {
 description : "selecting Key Pair"
 type = string
 }
+variable "vpc_id" {
+  description = "Existing VPC ID where EC2 and SG will be created"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Public Subnet ID where EC2 instance will be launched"
+  type        = string
+}
