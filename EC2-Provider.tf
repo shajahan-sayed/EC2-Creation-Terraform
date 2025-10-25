21148f159b954dfaeae542fd6f6834f191fc459e
shajahan-sayed <shajahansayed111@gmail.com>
2025-10-25 20:42:02 +0530

    Update EC2-Provider.tf

--- EC2-Provider.tf
+++ EC2-Provider.tf
@@ -1,9 +1,9 @@
 terraform {
     required_providers {
-         aws {
-            source  = "hashicorp/aws"
-             version = "~> 5.0"
-         }
+      aws {
+        source  = "hashicorp/aws"
+        version = "~> 5.0"
+      }
     }
 
   required_version = ">= 1.3.0"
