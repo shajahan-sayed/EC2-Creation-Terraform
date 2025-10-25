c9c46ba98955b0f3f0ca8415053e22862fd004ca
shajahan-sayed <shajahansayed111@gmail.com>
2025-10-25 19:05:50 +0530

    Create prod-main.tf

---
+++ prod-main.tf
@@ -0,0 +1,43 @@
+#create security group to allow SSH and Http
+resource "aws_security_group" "prod-main" {
+ name        = "prod-main"
+ description = "Allow SSH and HTTP"
+ vpc-id      =  vpc-0d5646eb0f4f2476a
+
+ ingress {
+  description = "Allow port SSH"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+ }
+ ingress {
+   description = "Allow port HTTP"
+   from_port  = 80
+   to_port    = 80
+   protocol   = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+}
+egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "prod-main"
+  }
+}
+#creating EC2 instance
+resource "aws_instance" "web-page"{
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = element(data.aws_subnet_ids.default.ids, 0)
+  key_name      = var.key_name
+  vpc_security_group_ids = [aws_security_group.prod-main.id]
+
+  tags = {
+    Name = "web-page"
+  }
+}
