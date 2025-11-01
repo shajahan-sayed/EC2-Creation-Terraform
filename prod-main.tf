b707b0b36897d9a8ca5374f86dd822587453261c
shajahan-sayed <shajahansayed111@gmail.com>
2025-11-01 13:36:36 +0530

    Update prod-main.tf

--- prod-main.tf
+++ prod-main.tf
@@ -3,7 +3,6 @@
 resource "aws_instance" "web-page"{
   ami           = var.ami_id
   instance_type = var.instance_type
-  key_name      = var.key_name
   associate_public_ip_address = true
 
   tags = {
